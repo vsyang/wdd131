9eb9e3275b2dc0713472b9d7cfa9dcd815d1c395
vsyang <yang.vanessa85@gmail.com>
2024-05-07 16:27:28 -0600

    ok now done maybe

--- scripts/place.js
+++ scripts/place.js
@@ -9,7 +9,6 @@ let wind = 10;
 const calculateWindChill = (temp, wind) => {
     if((temp <= 50 && wind > 3)) {
         
-
         const windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16));
         
         return `${windChill.toFixed(1)}°F`;
