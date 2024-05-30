091db66f25d48205b76d3b09a4087bc305b19fe7
vsyang <yang.vanessa85@gmail.com>
2024-05-29 22:36:13 -0600

    lazy loading implemented

--- project/scripts/gallery.js
+++ project/scripts/gallery.js
@@ -2,87 +2,121 @@ const images = [
     {
         size: "Extra Small",
         serve: "1-2",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/xsmall1.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/xsmall1.webp",
+        width: 300,
+        height: 240
     },
     {
         size: "Extra Small",
         serve: "1-2",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/xsmall2.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/xsmall2.webp",
+        width: 300,
+        height: 266
     },
     {
         size: "Small",
         serve: "3-10",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/small1.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/small1.webp",
+        width: 300,
+        height: 225
     },
     {
         size: "Small",
         serve: "3-10",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/small2.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/small2.webp",
+        width: 300,
+        height: 225
     },
     {
         size: "Small",
         serve: "3-10",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/small3.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/small3.webp",
+        width: 300,
+        height: 240
     },
     {
         size: "Small",
         serve: "3-10",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/small4.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/small4.webp",
+        width: 300,
+        height: 217
     },
     {
         size: "Small",
         serve: "3-10",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/small5.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/small5.webp",
+        width: 300,
+        height: 209
     },
     {
         size: "Medium",
         serve: "10-20",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium1.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium1.webp",
+        width: 300,
+        height: 225
     },
     {
         size: "Medium",
         serve: "10-20",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium2.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium2.webp",
+        width: 300,
+        height: 240
     },
     {
         size: "Medium",
         serve: "10-20",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium3.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium3.webp",
+        width: 300,
+        height: 158
     },
     {
         size: "Medium",
         serve: "10-20",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium4.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium4.webp",
+        width: 300,
+        height: 171
     },
     {
         size: "Medium",
         serve: "10-20",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium5.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/medium5.webp",
+        width: 300,
+        height: 175
     },
     {
         size: "Large",
         serve: "20-50",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/large1.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/large1.webp",
+        width: 300,
+        height: 139
     },
     {
         size: "Large",
         serve: "20-50",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/large2.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/large2.webp",
+        width: 300,
+        height: 252
     },
     {
         size: "Large",
         serve: "20-50",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/large3.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/large3.webp",
+        width: 300,
+        height: 240
     },
     {
         size: "Extra Large",
         serve: "100+",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/xlarge1.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/xlarge1.webp",
+        width: 300,
+        height: 225
     },
     {
         size: "Extra Large",
         serve: "100+",
-        imageUrl: "https://vsyang.github.io/wdd131/project/images/xlarge2.webp"
+        imageUrl: "https://vsyang.github.io/wdd131/project/images/xlarge2.webp",
+        width: 300,
+        height: 225
     },
 ];
 
@@ -99,6 +133,9 @@ const displayExamples = (images) => {
         let picture = document.createElement('img');
         picture.src = image.imageUrl;
         picture.alt = image.size;
+        picture.loading = 'lazy';
+        picture.width = image.width;
+        picture.height = image.height;
 
         let info = document.createElement('p');
         info.classList.add('info');
