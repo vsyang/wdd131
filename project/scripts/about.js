7ac5eed9b8b8725aef837fca7ca02a85fd9da28b
vsyang <yang.vanessa85@gmail.com>
2024-06-01 19:08:23 -0600

    small done. doing larger now

---
+++ project/scripts/about.js
@@ -0,0 +1,5 @@
+const menuLink = document.getElementById('cta');
+
+menuLink.addEventListener('click', () => {
+    window.location.href = 'https://vsyang.github.io/wdd131/project/gallery.html'
+});
